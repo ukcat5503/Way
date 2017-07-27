cd4d9dcd9275ae99f359e237e7158af1f1b3dadc
ukcat5503 <git@furuyama.tech>
2017-07-27 16:25:01 +0900

    見続けているかの判定を実装

--- Assets/Scripts/System/TrackingPoint.cs
+++ Assets/Scripts/System/TrackingPoint.cs
@@ -12,6 +12,13 @@ public class TrackingPoint : MonoBehaviour {
     /// マウスの向いている方向を表すRay
     /// </summary>
     Ray ray;
+
+    /// <summary>
+    /// 前フレームで選択していたオブジェクトのユニークID
+    /// </summary>
+    int prevHit = 0;
+    int count = 0;
+
     /// <summary>
     /// 選択対象のレイヤー
     /// </summary>
@@ -23,7 +30,19 @@ public class TrackingPoint : MonoBehaviour {
 
         RaycastHit hit;
         if(Physics.Raycast(ray, out hit, 1000.0f, TargetLayer)) {
+            if(prevHit == hit.collider.gameObject.GetInstanceID()){
+                Debug.Log("[" + ++count + "] selecting: " + hit.collider.gameObject.name);
+            }else{
+                prevHit = hit.collider.gameObject.GetInstanceID();
+            }
+
+
+
             hit.collider.GetComponent<MeshRenderer>().material.color = Color.red;
+            // TODO hitからスクリプトを得たいけどgetcomponent使いたくねぇよなぁ？
+            // CollisionObject a = hit.collider.gameObject;
+
+
             // hit.collider.enabled = false;
         }
     }
