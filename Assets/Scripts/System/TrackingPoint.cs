6b9d31a76b51b5a89ee2915392bf94330422ef84
ukcat5503 <git@furuyama.tech>
2017-07-27 13:53:22 +0900

    プレイヤーの視線の先から判定を得られるように

--- Assets/Scripts/System/TrackingPoint.cs
+++ Assets/Scripts/System/TrackingPoint.cs
@@ -2,12 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// カメラにアタッチするスクリプト。
+/// Rayを飛ばし、何と衝突しているかを判定できます。
+/// </summary>
 public class TrackingPoint : MonoBehaviour {
 
-    void Start()
-    {
+    /// <summary>
+    /// マウスの向いている方向を表すRay
+    /// </summary>
+    Ray ray;
 
+    void Update(){
+        ray = new Ray(transform.position, transform.forward);
+        Debug.DrawRay(ray.origin, ray.direction * 100, Color.red, 0f, true);
 
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit, 1000.0f)) {
+            hit.collider.GetComponent<MeshRenderer>().material.color = Color.red;
+            // hit.collider.enabled = false;
+        }
     }
-		
 }
