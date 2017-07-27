cadacc909704ca4de6d75cd90aeab5b6e4c137e7
ukcat5503 <git@furuyama.tech>
2017-07-27 14:27:49 +0900

    特定のレイヤーのみに判定が発生するように

---
+++ Assets/Scripts/Class/CollisionObject.cs
@@ -0,0 +1,18 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// [抽象クラス]
+/// 主人公の目線との当たり判定を必要とするオブジェクトはすべてこのクラスを継承します。
+/// 主人公と当たると.hit()が呼び出されます。
+/// </summary>
+// abstract 
+public class CollisionObject : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		gameObject.layer = 8;	// レイヤーをプレイヤーのレイキャストが有効なものにする
+	}
+	
+}
--- Assets/Scripts/System/TrackingPoint.cs
+++ Assets/Scripts/System/TrackingPoint.cs
@@ -12,13 +12,17 @@ public class TrackingPoint : MonoBehaviour {
     /// マウスの向いている方向を表すRay
     /// </summary>
     Ray ray;
+    /// <summary>
+    /// 選択対象のレイヤー
+    /// </summary>
+    public LayerMask TargetLayer;
 
     void Update(){
         ray = new Ray(transform.position, transform.forward);
         Debug.DrawRay(ray.origin, ray.direction * 100, Color.red, 0f, true);
 
         RaycastHit hit;
-        if(Physics.Raycast(ray, out hit, 1000.0f)) {
+        if(Physics.Raycast(ray, out hit, 1000.0f, TargetLayer)) {
             hit.collider.GetComponent<MeshRenderer>().material.color = Color.red;
             // hit.collider.enabled = false;
         }
