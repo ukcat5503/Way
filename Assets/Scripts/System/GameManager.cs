7b4ac4bcdcd8d3b7bad7cf7678d832751cd8036c
ukcat5503 <git@furuyama.tech>
2017-07-29 00:49:54 +0900

    FPSを起動時に固定

--- Assets/Scripts/System/GameManager.cs
+++ Assets/Scripts/System/GameManager.cs
@@ -36,6 +36,9 @@ public class GameManager : MonoBehaviour {
         get { return this.state; }
     }
 
+    void Awake(){
+        Application.targetFrameRate = 60;
+    }
 
 	// Use this for initialization
 	void Start () {
