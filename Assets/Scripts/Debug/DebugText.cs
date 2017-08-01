27a228fa61d4ab88f23101d3a010b7a5b59e7d8c
ukcat5503 <git@furuyama.tech>
2017-08-01 12:22:02 +0900

    デバッグ用表示をするクラスの拡張

--- Assets/Scripts/Debug/DebugText.cs
+++ Assets/Scripts/Debug/DebugText.cs
@@ -3,26 +3,38 @@ using UnityEngine.UI;
 using System;
 using System.Collections.Generic;
 
-[RequireComponent (typeof(Text))]
 /// <summary>
 /// デバッグ用 FSPを表示するだけ
 /// </summary>
 public class DebugText : MonoBehaviour
 {
-    Text text;
+    Text keyText;
+    Text valueText;
+
+    RectTransform valueRectTransform;
+
+    static int textLength = 0;
 
     static Dictionary<string, string> outputMessage = new Dictionary<string, string>();
 
     void Start(){
-        text = gameObject.GetComponent<Text>();
+        keyText =  gameObject.transform.FindChild("Key").GetComponent<Text>();
+        valueText =  gameObject.transform.FindChild("Value").GetComponent<Text>();
+        valueRectTransform = valueText.gameObject.GetComponent<RectTransform>();
     }
 
     void Update(){
-        string output = "";
+        string outputKey = "";
+        string outputValue = "";
+
         foreach (KeyValuePair<string, string> pair in outputMessage){
-            output += String.Format("{0, -20}",pair.Key + ": ") + pair.Value + "\n";
+            outputKey += pair.Key + ":\n";
+            outputValue += pair.Value + "\n";
         }
-        text.text = output;
+        keyText.text = outputKey;
+        valueText.text = outputValue;
+
+        valueRectTransform.localPosition = new Vector3(textLength * 30, -10, 0);
     }
 
     public static void RemoveInfo(string name){
@@ -30,10 +42,29 @@ public class DebugText : MonoBehaviour
     }
 
     public static void UpdateInfo(string key, string value){
-        if (outputMessage.ContainsKey(key) ){
+        if (outputMessage.ContainsKey(key)){
             outputMessage[key] = value;
         }else{
             outputMessage.Add(key, value);
+            textLength = key.Length > textLength ? key.Length : textLength;
+        }
+    }
+
+    public static void UpdateInfo(string key, int value){
+        if (outputMessage.ContainsKey(key) ){
+            outputMessage[key] = value.ToString();
+        }else{
+            outputMessage.Add(key, value.ToString());
+            textLength = key.Length > textLength ? key.Length : textLength;
+        }
+    }
+
+        public static void UpdateInfo(string key, float value){
+        if (outputMessage.ContainsKey(key) ){
+            outputMessage[key] = value.ToString();
+        }else{
+            outputMessage.Add(key, value.ToString());
+            textLength = key.Length > textLength ? key.Length : textLength;
         }
     }
 }
