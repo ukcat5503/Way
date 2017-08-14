e9a973c51203db045db8a48974fbfb0ef70c1bf9
ukcat5503 <git@furuyama.tech>
2017-08-14 16:17:16 +0900

    実際にプレイヤーが破壊するブロックを追加

---
+++ Assets/Scripts/Object/PuzzleBlock.cs
@@ -0,0 +1,34 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PuzzleBlock : ModelBase {
+
+	enum ColorName{
+		White,
+		Red,
+		Blue,
+		Green,
+		length
+	}
+
+	static readonly Dictionary<ColorName,Color32> Colors = new Dictionary<ColorName,Color32>() {
+		{ColorName.White, new Color32(255,255,255,255)},
+		{ColorName.Red, new Color32(175,0,0,255)},
+		{ColorName.Blue, new Color32(0,175,0,255)},
+		{ColorName.Green, new Color32(0,0,175,255)}
+    };
+
+	[SerializeField]
+	ColorName myColor;
+
+	// Use this for initialization
+	void Start () {
+		gameObject.GetComponent<MeshRenderer>().material.color = Colors[myColor];
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
