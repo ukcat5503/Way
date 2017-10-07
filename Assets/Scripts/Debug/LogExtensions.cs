7cc441334a10f1aad7f07dc884553d6ffe915a2c
ukcat5503 <git@furuyama.tech>
2017-10-08 04:11:22 +0900

    ボールが転がっている向きをキー操作で管理できるように

---
+++ Assets/Scripts/Debug/LogExtensions.cs
@@ -0,0 +1,68 @@
+using System.Diagnostics;
+using Debug = UnityEngine.Debug;
+
+/// <summary>
+/// 整形したテキストをDebug.Logで出力します。
+/// 実行環境では無効になり、関数の呼び出しも発生しません。
+/// </summary>
+public static class ForLogExtensions
+{
+    delegate void LogType(string s);
+    
+    /// <summary>
+    /// テキストを決められたフォーマットで整形します。
+    /// </summary>
+    /// <param name="colorCode">Unityリッチテキストで使用できるカラー指定子、またはアルファ値込みカラーコード(#ffffffff)</param>
+    /// <returns>整形後の文字列</returns>
+    private static string formatText(string colorCode){
+        return "<color=" + colorCode + "><b>[{0}]</b></color> {1}";
+    }
+
+
+    [Conditional("UNITY_EDITOR")]
+    /// <summary>
+    /// Debug.Logを実行します。
+    /// 型によって自動的に色が設定されます。
+    /// </summary>
+    /// <param name="t">出力したいオブジェクト</param>
+    public static void Log(this object t){
+        output(t, Debug.Log);
+    }
+
+    [Conditional("UNITY_EDITOR")]
+    /// <summary>
+    /// Debug.LogWarningを実行します。
+    /// 型によって自動的に色が設定されます。
+    /// </summary>
+    /// <param name="t">出力したいオブジェクト</param>
+    public static void LogWarning(this object t){
+        output(t, Debug.LogWarning);
+    }
+
+    [Conditional("UNITY_EDITOR")]
+    /// <summary>
+    /// Debug.LogErrorを実行します。
+    /// 型によって自動的に色が設定されます。
+    /// </summary>
+    /// <param name="t">出力したいオブジェクト</param>
+    public static void LogError(this object t){
+        output(t, Debug.LogError);
+    }
+
+    [Conditional("UNITY_EDITOR")]
+    /// <summary>
+    /// 出力する根底のスクリプト
+    /// </summary>
+    /// <param name="t"></param>
+    private static void output(object t, LogType type){
+        if(t is UnityEngine.Vector3){
+            type(string.Format(formatText("Blue"), "Vector3", t));
+        }else if(t is UnityEngine.Quaternion){
+            type(string.Format(formatText("Yellow"), "Quaternion", t));
+        }else if(t is string){
+            type(string.Format(formatText("Black"), "string", t));
+        }else{
+            type(string.Format(formatText("#333333ff"), "Other", t));
+        }
+    }
+}
--- Assets/Scripts/Sphere.cs
+++ Assets/Scripts/Sphere.cs
@@ -4,14 +4,44 @@ using UnityEngine;
 
 public class Sphere : MonoBehaviour {
 
+	Rigidbody rigidbody;
+
+	GameObject parent;
+
+	const float kSpeed = 3f;
+
+	int frame = 0;
+
 	// Use this for initialization
 	void Start () {
+		rigidbody = GetComponent<Rigidbody>();
+
+		parent = transform.root.gameObject;
 		
+		(transform.forward * kSpeed).Log();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-		}
+        if (Input.GetKey (KeyCode.UpArrow)) {
+            rigidbody.AddForce(0,0,kSpeed);
+        }
+        if (Input.GetKey (KeyCode.DownArrow)) {
+            rigidbody.AddForce(0,0,-kSpeed);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+			var e = parent.transform.eulerAngles;
+			e.y += kSpeed;
+            parent.transform.eulerAngles = e;
+        }
+        if (Input.GetKey(KeyCode.RightArrow)) {
+			var e = parent.transform.eulerAngles;
+			e.y -= kSpeed;
+            parent.transform.eulerAngles = e;
+        }
+
+		parent.transform.position += transform.localPosition;
+		transform.localPosition = Vector3.zero;
 	}
 }
