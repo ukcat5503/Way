edd26c0449e4b089881a4313ae2d3eaf933c3f42
ukcat5503 <git@furuyama.tech>
2017-10-13 00:32:11 +0900

    SphereをAddForceではなく 直接操作するように変更

---
+++ Assets/Scripts/SphereController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereController : MonoBehaviour {
+
+	GameObject child;
+
+	Rigidbody rigidBody;
+
+	const float kSpeedPerSecond = 2.5f;
+
+	[SerializeField]
+	float rotateY = 0;
+	public float RotateY{
+		get
+		{
+			return rotateY;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+		child = transform.FindChild("SphereBody").gameObject;
+		RotationY(180f);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		child.transform.Rotate(kSpeedPerSecond * 2f, 0, 0);
+
+		var radian = (transform.eulerAngles.y) * Mathf.Deg2Rad;
+		Vector2 vector = new Vector2(Mathf.Sin(radian), Mathf.Cos(radian));
+		transform.position = new Vector3(
+			(vector.x * kSpeedPerSecond / 60f) + transform.position.x,
+			transform.position.y,
+			(vector.y * kSpeedPerSecond / 60f) + transform.position.z
+		);
+	}
+
+	public void RotationY(float rotate)
+	{
+		rotateY += rotate;
+		transform.Rotate(0, rotate, 0);
+	}
+}
