543fee81ee83bd69d9024b20810c34db1a63f75b
ukcat5503 <git@furuyama.tech>
2017-10-15 01:46:57 +0900

    前回のファイルをコピー

---
+++ Assets/Scripts/Object/NoteObject.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteObject : SpriteBase {
+
+	[SerializeField]
+	SoundManager.SE se;
+
+	SpriteRenderer sr;
+	
+	int frame = 0;
+
+	void Start () {
+		sr = GetComponent<SpriteRenderer>();
+		sr.enabled = false;
+		
+	}
+	
+	void Update () {
+		base.Update();
+
+		++frame;
+	}
+
+	void OnTriggerEnter(Collider other){
+		/*
+		if(frame > 120){
+			sr.enabled = true;
+			frame = 0;
+			SoundManager.PlaySE(se);
+
+		}
+		*/
+		if(sr.enabled == false){
+			sr.enabled = true;
+			SoundManager.PlaySE(se);
+		}
+
+	}
+}
---
+++ Assets/Scripts/System/SoundManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour {
+
+	public enum SE
+	{
+		GenerateBuilding,
+		ShotPlayer,
+		_1_do,
+		_2_re,
+		_3_mi,
+		_4_fa,
+		_5_so,
+		_6_ra,
+		_7_si,
+		_8_do
+	}
+
+	public enum BGM
+	{
+		RunTrolley
+	}
+
+	static List<AudioClip> se = new List<AudioClip>();
+	static List<AudioClip> bgm = new List<AudioClip>();
+
+	static AudioSource audioSource;
+
+	// Use this for initialization
+	void Awake () {
+		audioSource = GetComponent<AudioSource>();
+
+		foreach (SE key in SE.GetValues(typeof(SE)))
+		{
+			se.Add(Resources.Load("Audio/SE/" + key.ToString()) as AudioClip);
+		}
+
+		foreach (BGM key in BGM.GetValues(typeof(BGM)))
+		{
+			bgm.Add(Resources.Load("Audio/BGM/" + key.ToString()) as AudioClip);
+		}
+
+	}
+
+	void Start(){
+		// PlayBGM(BGM.RunTrolley);
+	}
+
+	public static void PlaySE(SE play){
+		audioSource.PlayOneShot(se[(int)play], 1f);
+	}
+
+	public static void PlayBGM(BGM play){
+		audioSource.clip = bgm[(int)play];
+		audioSource.Play();
+	}	
+}
