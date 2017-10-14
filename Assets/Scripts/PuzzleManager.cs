65a94740a2176e11199fdd5745b98cc1b95817a4
ukcat5503 <git@furuyama.tech>
2017-10-15 01:44:09 +0900

    オブジェクトを生成するベース機能を追加

--- Assets/Scripts/PuzzleManager.cs
+++ Assets/Scripts/PuzzleManager.cs
@@ -3,20 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PuzzleManager : MonoBehaviour {
+	class ObjectInfo{
+		public Vector3 pos;
+		public int obj;
+
+		public ObjectInfo(float x, float y, float z, int objectNumber){
+			pos = new Vector3(x,y,z);
+			obj = objectNumber;
+		}
+	}
 
 	int[,,] map;
+	List<ObjectInfo> objInfo = new List<ObjectInfo>();
 
 	[SerializeField]
 	GameObject[] GenerateBlocks;
+	[SerializeField]
+	GameObject[] GenerateObjects;
 	[SerializeField, Space(6)]
 	GameObject SphereController;
+	
 
 	GameObject cameraObject;
 
-	List<Vector3> generateSpherePos = new List<Vector3>();
-
 	// Use this for initialization
 	void Start () {
+		initialize();
+
+		cameraObject = GameObject.Find("Main Camera");
+		cameraObject.transform.position = new Vector3(map.GetLength(2) / 2f - 0.5f, map.GetLength(0) + 0.5f, map.GetLength(1) / 2f - 0.5f);
+	}
+
+	void initialize(){
 		map = new int[3,5,5]{
 			{
 				{4,2,2,2,5},
@@ -41,12 +59,11 @@ public class PuzzleManager : MonoBehaviour {
 			}
 		};
 
-		cameraObject = GameObject.Find("Main Camera");
-		cameraObject.transform.position = new Vector3(map.GetLength(2) / 2f - 0.5f, map.GetLength(0) + 0.5f, map.GetLength(1) / 2f - 0.5f);
+		objInfo.Clear();
+		objInfo.Add(new ObjectInfo(4,4,3,0));
 
 		for (int y = map.GetLength(0) - 1; y >= 0; --y)
 		{
-			
 			for (int z = map.GetLength(1) - 1; z >= 0; --z)
 			{
 				for (int x = map.GetLength(2) - 1; x >= 0; --x)
