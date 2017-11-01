32ae9db07cf0d4c89e0d3c68773cb12105c5dc0f
ukcat5503 <git@furuyama.tech>
2017-11-02 01:00:46 +0900

    手持ちブロック？の要素を追加(挙動が若干ふざけてるけど・・・)

---
+++ Assets/Scripts/Objects/HeldBlockParent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeldBlockParent : MonoBehaviour {
+
+	const float kDestroyHeight = -2f;
+
+	void Update () {
+		if(transform.position.y < kDestroyHeight){
+			Destroy(gameObject);
+		}
+	}
+}
---
+++ Assets/Scripts/System/HeldBlockManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeldBlockManager : MonoBehaviour {
+
+	[SerializeField]
+	GameObject blocksParentObject;
+	[SerializeField]
+	GameObject parentPrefab;
+	[SerializeField]
+	GameObject[] childPrefabs;
+
+	int frame = 0;
+
+	void Update () {
+		if(++frame % 30 == 0){
+			var pos = new Vector3(-4.5f, 0f, 10f);
+			var parent = Instantiate(parentPrefab, pos, Quaternion.identity) as GameObject;
+			var child = Instantiate(childPrefabs[0], pos, childPrefabs[0].transform.rotation);
+			parent.transform.parent = blocksParentObject.transform;
+			child.transform.parent = parent.transform;
+
+			float angle = Random.Range(130f,230f);
+			var shotVector = new Vector3(Mathf.Sin(angle * Mathf.Deg2Rad),0f , Mathf.Cos(angle * Mathf.Deg2Rad));
+			shotVector.Log();
+			parent.GetComponent<Rigidbody>().AddForce(shotVector * 100f);
+		}
+		
+	}
+}
