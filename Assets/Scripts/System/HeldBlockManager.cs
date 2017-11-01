10434154f8015be523408db9737840be2b4e1703
ukcat5503 <git@furuyama.tech>
2017-11-02 02:21:07 +0900

    生成した手持ちブロックが重なり合ってすっ飛んでいくのを防止する機能を追加

--- Assets/Scripts/System/HeldBlockManager.cs
+++ Assets/Scripts/System/HeldBlockManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class HeldBlockManager : MonoBehaviour {
 
 	[SerializeField]
+	LayerMask blockParentLayerMask;
+	[SerializeField]
 	GameObject blocksParentObject;
 	[SerializeField]
 	GameObject parentPrefab;
@@ -14,17 +16,29 @@ public class HeldBlockManager : MonoBehaviour {
 	int frame = 0;
 
 	void Update () {
-		if(++frame % 30 == 0){
-			var pos = new Vector3(Random.Range(-2.5f,-6.5f), 0f, 12f);
+		if(++frame % 20 == 0){
+			Vector3 pos;
+			{
+				Collider[] objs;
+				int count = 0;
+				do{
+					if(++count > 5){
+						"どうしても空いていなかったのでスキップしたよ".Log();
+						return;
+					}
+					pos = new Vector3(Random.Range(-2.5f,-6.5f), 0f, 11.5f);
+					objs = Physics.OverlapBox(pos, new Vector3(0.5f, 0.5f, 0.5f), Quaternion.identity, blockParentLayerMask);
+				} while (objs.Length > 0);
+			}
+
 			var parent = Instantiate(parentPrefab, pos, Quaternion.identity) as GameObject;
 			var child = Instantiate(childPrefabs[0], pos, childPrefabs[0].transform.rotation);
 			parent.transform.parent = blocksParentObject.transform;
 			child.transform.parent = parent.transform;
 
-			// float angle = Random.Range(130f,230f);
+			// float angle = Random.Range(150f,210f);
 			float angle = 180f;
 			var shotVector = new Vector3(Mathf.Sin(angle * Mathf.Deg2Rad),0f , Mathf.Cos(angle * Mathf.Deg2Rad));
-			shotVector.Log();
 			parent.GetComponent<Rigidbody>().AddForce(shotVector * 100f);
 		}
 		
