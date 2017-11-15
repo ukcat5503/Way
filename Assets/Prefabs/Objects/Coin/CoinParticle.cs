883249581399dc9b56219d16fee6e7dfeca4351a
ukcat5503 <git@furuyama.tech>
2017-11-16 01:02:31 +0900

    コイン獲得時のparticleを追加

---
+++ Assets/Prefabs/Objects/Coin/CoinParticle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinParticle : MonoBehaviour {
+
+	[SerializeField]
+	GameObject flyTargetObject;
+	[SerializeField]
+	GameObject particlePrefab;
+
+	[SerializeField]
+	public float coin;
+
+	const float coinPerParticle = 0.1f;
+
+	void Start () {
+		int particleQty = (int)(coin / coinPerParticle);
+		for (int i = 0; i < particleQty; ++i){
+			var obj = Instantiate(particlePrefab) as GameObject;
+			float angle = Random.Range(0f,360f);
+			var shotVector = new Vector3(Mathf.Sin(angle * Mathf.Deg2Rad),0f , Mathf.Cos(angle * Mathf.Deg2Rad));
+			obj.GetComponent<Rigidbody>().AddForce(shotVector * 0.5f, ForceMode.Impulse);
+		}
+	}
+}
