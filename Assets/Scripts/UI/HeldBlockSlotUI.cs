893a6ef254fe95b3922dbe017f0c47c62ae00063
ukcat5503 <git@furuyama.tech>
2017-11-20 00:16:19 +0900

    UIをクリックする機能を仮追加

--- Assets/Scripts/UI/HeldBlockSlotUI.cs
+++ Assets/Scripts/UI/HeldBlockSlotUI.cs
@@ -6,8 +6,6 @@ public class HeldBlockSlotUI : MonoBehaviour {
 
 	RectTransform[] SlotObject;
 
-
-
 	[SerializeField]
 	int slotSize = 6;
 	[SerializeField]
@@ -21,12 +19,12 @@ public class HeldBlockSlotUI : MonoBehaviour {
 			SlotObject[i] = (Instantiate(slotPrefab, new Vector3(230f, -110f + -(i * 150), 0), Quaternion.identity) as GameObject).GetComponent<RectTransform>();
 			SlotObject[i].SetParent(transform, false);
 			SlotObject[i].transform.name = "BlockSlot " + i;
-
-			// 230 -140 
 		}
 	}
 	
 	void Update () {
 		
 	}
+
+
 }
