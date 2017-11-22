ad63188f49f5a073c7110ed2a4e728c656a81fdd
ukcat5503 <git@furuyama.tech>
2017-11-22 12:29:37 +0900

    手持ちブロックスロットの当たり判定を追加

--- Assets/Scripts/UI/HeldBlockSlotUI.cs
+++ Assets/Scripts/UI/HeldBlockSlotUI.cs
@@ -7,23 +7,50 @@ public class HeldBlockSlotUI : MonoBehaviour {
 	RectTransform[] SlotObject;
 
 	[SerializeField]
-	int slotSize = 6;
+	int slotLength;
 	[SerializeField]
 	GameObject slotPrefab;
 
+	[SerializeField]
+	Vector2 basePosition, slotSize, positionMargin;
+
+	Rect[] buttonPosition;
+
+	[SerializeField]
+	GameObject rect;
+
 	void Start () {
-		SlotObject = new RectTransform[slotSize];
+		SlotObject = new RectTransform[slotLength];
+		buttonPosition = new Rect[slotLength];
 
 		var length = SlotObject.Length;
 		for (int i = 0; i < length; ++i){
 			SlotObject[i] = (Instantiate(slotPrefab, new Vector3(230f, -110f + -(i * 150), 0), Quaternion.identity) as GameObject).GetComponent<RectTransform>();
 			SlotObject[i].SetParent(transform, false);
 			SlotObject[i].transform.name = "BlockSlot " + i;
+			buttonPosition[i] = new Rect(basePosition.x + (positionMargin.x * i), basePosition.y + (positionMargin.y * i), slotSize.x, slotSize.y);
+
+			if (true)
+			{
+				var obj = (Instantiate(rect, new Vector3(buttonPosition[i].x, buttonPosition[i].y, 0), Quaternion.identity) as GameObject).GetComponent<RectTransform>();
+				obj.parent = transform.parent;
+				obj.sizeDelta = new Vector2(slotSize.x, slotSize.y);
+			}
 		}
 	}
 	
 	void Update () {
-		
+		if (Input.GetMouseButtonDown(0))
+		{
+			var length = buttonPosition.Length;
+			for (int i = 0; i < length; ++i)
+			{
+				if (buttonPosition[i].Contains(Input.mousePosition))
+				{
+					("クリック obj:" + i).Log();
+				}
+			}
+		}
 	}
 
 
