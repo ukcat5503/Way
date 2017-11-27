133d7d487fc12ad6c1d8034e6c7d2ab238f3f2bf
ukcat5503 <git@furuyama.tech>
2017-11-28 01:28:45 +0900

    ルーレットを使ってブロックを配置できるように

--- Assets/Scripts/UI/HeldBlockSlotUI.cs
+++ Assets/Scripts/UI/HeldBlockSlotUI.cs
@@ -20,8 +20,15 @@ public class HeldBlockSlotUI : MonoBehaviour {
 	Color normalStateColor, pickingStateColor;
 
 	// 選択中のブロック情報
-	Vector2 pickingWorldPos;
 	bool isPicking;
+	int currentObj;
+	Vector3 pickingObject;
+
+	// ブロックのホイール
+	Image imageTop2, imageTop1, image0, imageUnder1, imageUnder2;
+	const float kWheelSensitivity = 0f;
+	float wheelValue;
+
 
 	[Space(8), SerializeField]
 	Sprite[] heldObjSprite;
@@ -37,6 +44,18 @@ public class HeldBlockSlotUI : MonoBehaviour {
 		cursorGuideObject = Instantiate(cursorGuideObject) as GameObject;
 		cursorGuideMeshRenderer = cursorGuideObject.GetComponentInChildren<MeshRenderer>();
 		cursorGuideMeshRenderer.material.color = normalStateColor;
+
+		imageTop2 = transform.Find("currentObj+2").GetComponent<Image>();
+		imageTop1 = transform.Find("currentObj+1").GetComponent<Image>();
+		image0 = transform.Find("currentObj").GetComponent<Image>();
+		imageUnder1 = transform.Find("currentObj-1").GetComponent<Image>();
+		imageUnder2 = transform.Find("currentObj-2").GetComponent<Image>();
+
+		imageTop2.sprite = heldObjSprite[calcFixedIndex(currentObj - 2)];
+		imageTop1.sprite = heldObjSprite[calcFixedIndex(currentObj - 1)];
+		image0.sprite = heldObjSprite[calcFixedIndex(currentObj)];
+		imageUnder1.sprite = heldObjSprite[calcFixedIndex(currentObj + 1)];
+		imageUnder2.sprite = heldObjSprite[calcFixedIndex(currentObj + 2)];
 	}
 	
 	void Update () {
@@ -65,17 +84,52 @@ public class HeldBlockSlotUI : MonoBehaviour {
 				cursorGuideObject.transform.position = new Vector3(-50f, -50f, -50f);
 				Cursor.visible = true;
 			}
+		}else{
+			wheelValue += Input.GetAxis("Mouse ScrollWheel");
+			if(wheelValue > kWheelSensitivity){
+				wheelValue = 0f;
+				wheelScrollImage(true);
+
+			}else if(wheelValue < -kWheelSensitivity){
+				wheelValue = 0f;
+				wheelScrollImage();
+			}
 		}
 
 		// 最初にUIクリック
 		if(Input.GetMouseButtonDown(0) && !isPicking){
 			isPicking = true;
 			cursorGuideMeshRenderer.material.color = pickingStateColor;
+			pickingObject = mouseWorldPosition;
+
+			transform.position = RectTransformUtility.WorldToScreenPoint (Camera.main, mouseWorldPosition);
 		}else if(Input.GetMouseButtonDown(0) && isPicking){
 			isPicking = false;
 			cursorGuideMeshRenderer.material.color = normalStateColor;
 
+
+			var obj = Instantiate(heldObject[calcFixedIndex(currentObj)], pickingObject, heldObject[calcFixedIndex(currentObj)].transform.rotation) as GameObject;
+
+			transform.position = RectTransformUtility.WorldToScreenPoint (Camera.main, new Vector3(-50f, -50f, -50f));
+
+			
 		}
+	}
+
+	void wheelScrollImage(bool isMoveUp = false){
+		if(isMoveUp)	++currentObj;
+		else			--currentObj;
+
+		imageTop2.sprite = heldObjSprite[calcFixedIndex(currentObj - 2)];
+		imageTop1.sprite = heldObjSprite[calcFixedIndex(currentObj - 1)];
+		image0.sprite = heldObjSprite[calcFixedIndex(currentObj)];
+		imageUnder1.sprite = heldObjSprite[calcFixedIndex(currentObj + 1)];
+		imageUnder2.sprite = heldObjSprite[calcFixedIndex(currentObj + 2)];
+	}
+
+	int calcFixedIndex(int index){
+		var i = index % heldObjSprite.Length;
+		return i >= 0 ? i : heldObjSprite.Length + i;
 	}
 
 	bool isContainLocalMap(Vector3 localPos){
