f1e750f59edf7327e1649faecd56b9790b85c30e
ukcat5503 <git@furuyama.tech>
2017-11-29 02:07:44 +0900

    ゴール時 プレイヤーを削除するように

--- Assets/Scripts/Blocks/GoalBlock.cs
+++ Assets/Scripts/Blocks/GoalBlock.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class GoalBlock : MonoBehaviour {
 	void OnTriggerEnter(Collider other){
+		Destroy(other.gameObject);
 		PuzzleManager.NextStage(transform.parent.parent.gameObject);
 	}
 }
