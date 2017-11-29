542ab0ef7dbeb881ad18311729366ef98c455764
ukcat5503 <git@furuyama.tech>
2017-11-29 23:32:58 +0900

    チュートリアルの吹き出し素材と処理を追加

---
+++ Assets/Scripts/UI/VoiceWindow.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VoiceWindow : MonoBehaviour {
+	Text text;
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponentInChildren<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = transform.name;
+	}
+}
