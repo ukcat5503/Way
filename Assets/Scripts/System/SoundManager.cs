4c14b3ca8ddd8cf7c2299ba10d2d5e57543be4bc
ukcat5503 <git@furuyama.tech>
2017-11-30 10:01:42 +0900

    SEを追加

--- Assets/Scripts/System/SoundManager.cs
+++ Assets/Scripts/System/SoundManager.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SoundManager : MonoBehaviour {
+public class SoundManager : MonoBehaviour
+{
 
 	public enum SE
 	{
 		select,
 		push,
-		cancel
+		cancel,
+		move
 	}
 
 	public enum BGM
@@ -16,14 +18,14 @@ public class SoundManager : MonoBehaviour {
 		Blue_Ever
 	}
 
-
 	static List<AudioClip> se = new List<AudioClip>();
 	static List<AudioClip> bgm = new List<AudioClip>();
 
 	static AudioSource audioSource;
 
 	// Use this for initialization
-	void Awake () {
+	void Awake()
+	{
 		audioSource = GetComponent<AudioSource>();
 
 		foreach (SE key in SE.GetValues(typeof(SE)))
@@ -38,16 +40,19 @@ public class SoundManager : MonoBehaviour {
 
 	}
 
-	void Start(){
+	void Start()
+	{
 		PlayBGM(BGM.Blue_Ever);
 	}
 
-	public static void PlaySE(SE play){
+	public static void PlaySE(SE play)
+	{
 		audioSource.PlayOneShot(se[(int)play], 1f);
 	}
 
-	public static void PlayBGM(BGM play){
+	public static void PlayBGM(BGM play)
+	{
 		audioSource.clip = bgm[(int)play];
 		audioSource.Play();
-	}	
+	}
 }
