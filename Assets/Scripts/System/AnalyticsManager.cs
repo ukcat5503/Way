236e7db4fdd057964635a38c960e371d81c9fbc8
ukcat5503 <git@furuyama.tech>
2017-12-04 00:34:11 +0900

    Google Analiticsを管理するクラスを追加

---
+++ Assets/Scripts/System/AnalyticsManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnalyticsManager : MonoBehaviour {
+
+	[SerializeField]
+	GameObject GAv4Prefab;
+	static GoogleAnalyticsV4 googleAnalytics;
+
+	// Use this for initialization
+	void Start () {
+		googleAnalytics = (Instantiate(GAv4Prefab) as GameObject).GetComponent<GoogleAnalyticsV4>();
+	}
+
+	public static void StartSession(){
+		googleAnalytics.StartSession();
+	}
+
+	public static void StopSession(){
+		googleAnalytics.StopSession();
+	}
+	
+	public static void LogScreen(string eventName){
+		googleAnalytics.LogScreen(eventName);
+	}
+	
+	public static void LogEvent(string eventCategory, string eventAction, string eventLabel, long value){
+		googleAnalytics.LogEvent(eventCategory, eventAction, eventLabel, value);
+	}
+}
