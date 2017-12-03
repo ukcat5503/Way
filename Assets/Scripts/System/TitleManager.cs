32d78ccff3c042d24a144631aa61f7e024f085ef
ukcat5503 <git@furuyama.tech>
2017-12-04 00:04:31 +0900

    フェードイン処理を追加

--- Assets/Scripts/System/TitleManager.cs
+++ Assets/Scripts/System/TitleManager.cs
@@ -4,9 +4,17 @@ using UnityEngine.SceneManagement;
 
 public class TitleManager : MonoBehaviour {
 
+	enum TitleState{
+		FadeIn,
+		Waiting,
+		FadeOut
+	}
+
 	Image _image;
 
 	float timeElapsed;
+
+	TitleState state;
 
 	string nextSceneName = "";
 
@@ -19,31 +27,47 @@ public class TitleManager : MonoBehaviour {
 	string logoSceneName;
 
 	void Start(){
+		state = TitleState.FadeIn;
 		_image = GameObject.Find(ImageObjName).GetComponent<Image>();
+		_image.color = new Color(_image.color.r, _image.color.g, _image.color.b, 1f);
 	}
 
 	void Update () {
 		timeElapsed += Time.deltaTime;
 		timeElapsed.Log();
 
-		if(nextSceneName == "" && ( Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2))){
+		if(state == TitleState.Waiting && ( Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1) || Input.GetMouseButtonDown(2))){
 			// ゲームへ
-			"ゲームへ".Log();
 			nextSceneName = gameSceneName;
-		}else if(nextSceneName == "" && timeElapsed > 60){
+			state = TitleState.FadeOut;
+		}else if(state == TitleState.Waiting && timeElapsed > 60){
 			// 学校ロゴへ
 			nextSceneName = logoSceneName;
+			state = TitleState.FadeOut;
 		}
 
-		if(nextSceneName != ""){
-			blackOut();
+		if(state == TitleState.FadeIn){
+			fadeIn();
+		}
+		if(state == TitleState.FadeOut){
+			fadeOut();
 		}
 	}
 
-	void blackOut(){
-		var c = _image.color.a;
-		c += 1f / 60f;
-		_image.color = new Color(0, 0, 0, c);
+	void fadeIn(){
+		var a = _image.color.a;
+		a -= 1f / 60f;
+		_image.color = new Color(_image.color.r, _image.color.g, _image.color.b, a);
+
+		if(_image.color.a < 0){
+			state = TitleState.Waiting;
+		}
+	}
+
+	void fadeOut(){
+		var a = _image.color.a;
+		a += 1f / 60f;
+		_image.color = new Color(_image.color.r, _image.color.g, _image.color.b, a);
 
 		if(_image.color.a > 1){
 			SceneManager.LoadScene(nextSceneName);
