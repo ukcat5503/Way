e8245c57675c8da36df6afd4fb96fa2912ff15f4
ukcat5503 <git@furuyama.tech>
2017-12-07 03:25:03 +0900

    親オブジェクトの回転に合わせてスクリプトを修正

--- Assets/Scripts/Objects/Tutorial/Tutorial_Select.cs
+++ Assets/Scripts/Objects/Tutorial/Tutorial_Select.cs
@@ -29,69 +29,69 @@ public class Tutorial_Select : MonoBehaviour {
 		if(frame > 90 && frame <= 120){
 			rollButton.enabled = true;
 			
-			blocks[1].transform.position = new Vector3(blocks[1].transform.position.x, blocks[1].transform.position.y + (0.6f / 30f), blocks[1].transform.position.z);
-			blocks[2].transform.position = new Vector3(blocks[2].transform.position.x, blocks[2].transform.position.y + (0.6f / 30f), blocks[2].transform.position.z);
-			allow.transform.position = new Vector3(allow.transform.position.x, allow.transform.position.y + (1f / 30f), allow.transform.position.z);
+			blocks[1].transform.position = new Vector3(blocks[1].transform.position.x, blocks[1].transform.position.y, blocks[1].transform.position.z + (0.6f / 30f));
+			blocks[2].transform.position = new Vector3(blocks[2].transform.position.x, blocks[2].transform.position.y, blocks[2].transform.position.z + (0.6f / 30f));
+			allow.transform.position = new Vector3(allow.transform.position.x, allow.transform.position.y, allow.transform.position.z + (1f / 30f));
 
 			if(frame < 105){
 				blocks[0].color = new Color(blocks[0].color.r, blocks[0].color.g, blocks[0].color.b, blocks[0].color.a - (1f / 15f));
 				allow.color = new Color(allow.color.r, allow.color.g, allow.color.b, allow.color.a + (1f / 15f));
 			}else if(frame == 105){
 				blocks[0].color = new Color(blocks[0].color.r, blocks[0].color.g, blocks[0].color.b, 0);
-				blocks[0].transform.position = new Vector3(blocks[0].transform.position.x, blocks[0].transform.position.y - (0.6f * 2f), blocks[0].transform.position.z);
+				blocks[0].transform.position = new Vector3(blocks[0].transform.position.x, blocks[0].transform.position.y, blocks[0].transform.position.z - (0.6f * 2f));
 			}else{
 				blocks[0].color = new Color(blocks[0].color.r, blocks[0].color.g, blocks[0].color.b, blocks[0].color.a + (1f / 15f));
 				allow.color = new Color(allow.color.r, allow.color.g, allow.color.b, allow.color.a - (1f / 15f));
 			}
 
 			if(frame == 120){
-				allow.transform.position = new Vector3(allow.transform.position.x, allow.transform.position.y - 1f, allow.transform.position.z);
+				allow.transform.position = new Vector3(allow.transform.position.x, allow.transform.position.y, allow.transform.position.z - 1f);
 				rollButton.enabled = false;
 			}
 		}
 
 		if(frame > 210 && frame <= 240){
 			rollButton.enabled = true;
-			blocks[2].transform.position = new Vector3(blocks[2].transform.position.x, blocks[2].transform.position.y + (0.6f / 30f), blocks[2].transform.position.z);
-			blocks[0].transform.position = new Vector3(blocks[0].transform.position.x, blocks[0].transform.position.y + (0.6f / 30f), blocks[0].transform.position.z);
-			allow.transform.position = new Vector3(allow.transform.position.x, allow.transform.position.y + (1f / 30f), allow.transform.position.z);
+			blocks[2].transform.position = new Vector3(blocks[2].transform.position.x, blocks[2].transform.position.y, blocks[2].transform.position.z + (0.6f / 30f));
+			blocks[0].transform.position = new Vector3(blocks[0].transform.position.x, blocks[0].transform.position.y, blocks[0].transform.position.z + (0.6f / 30f));
+			allow.transform.position = new Vector3(allow.transform.position.x, allow.transform.position.y, allow.transform.position.z + (1f / 30f));
 
 			if(frame < 225){
 				blocks[1].color = new Color(blocks[1].color.r, blocks[1].color.g, blocks[1].color.b, blocks[1].color.a - (1f / 15f));
 				allow.color = new Color(allow.color.r, allow.color.g, allow.color.b, allow.color.a + (1f / 15f));
 			}else if(frame == 225){
 				blocks[1].color = new Color(blocks[1].color.r, blocks[1].color.g, blocks[1].color.b, 0);
-				blocks[1].transform.position = new Vector3(blocks[1].transform.position.x, blocks[1].transform.position.y - (0.6f * 2f), blocks[1].transform.position.z);
+				blocks[1].transform.position = new Vector3(blocks[1].transform.position.x, blocks[1].transform.position.y, blocks[1].transform.position.z - (0.6f * 2f));
 			}else{
 				blocks[1].color = new Color(blocks[1].color.r, blocks[1].color.g, blocks[1].color.b, blocks[1].color.a + (1f / 15f));
 				allow.color = new Color(allow.color.r, allow.color.g, allow.color.b, allow.color.a - (1f / 15f));
 			}
 
 			if(frame == 240){
-				allow.transform.position = new Vector3(allow.transform.position.x, allow.transform.position.y - 1f, allow.transform.position.z);
+				allow.transform.position = new Vector3(allow.transform.position.x, allow.transform.position.y, allow.transform.position.z - 1f);
 				rollButton.enabled = false;
 			}
 		}
 
 		if(frame > 330 && frame <= 360){
 			rollButton.enabled = true;
-			blocks[0].transform.position = new Vector3(blocks[0].transform.position.x, blocks[0].transform.position.y + (0.6f / 30f), blocks[0].transform.position.z);
-			blocks[1].transform.position = new Vector3(blocks[1].transform.position.x, blocks[1].transform.position.y + (0.6f / 30f), blocks[1].transform.position.z);
-			allow.transform.position = new Vector3(allow.transform.position.x, allow.transform.position.y + (1f / 30f), allow.transform.position.z);
+			blocks[0].transform.position = new Vector3(blocks[0].transform.position.x, blocks[0].transform.position.y, blocks[0].transform.position.z + (0.6f / 30f));
+			blocks[1].transform.position = new Vector3(blocks[1].transform.position.x, blocks[1].transform.position.y, blocks[1].transform.position.z + (0.6f / 30f));
+			allow.transform.position = new Vector3(allow.transform.position.x, allow.transform.position.y, allow.transform.position.z + (1f / 30f));
 
 			if(frame < 345){
 				blocks[2].color = new Color(blocks[2].color.r, blocks[2].color.g, blocks[2].color.b, blocks[2].color.a - (1f / 15f));
 				allow.color = new Color(allow.color.r, allow.color.g, allow.color.b, allow.color.a + (1f / 15f));
 			}else if(frame == 345){
 				blocks[2].color = new Color(blocks[2].color.r, blocks[2].color.g, blocks[2].color.b, 0);
-				blocks[2].transform.position = new Vector3(blocks[2].transform.position.x, blocks[2].transform.position.y - (0.6f * 2f), blocks[2].transform.position.z);
+				blocks[2].transform.position = new Vector3(blocks[2].transform.position.x, blocks[2].transform.position.y, blocks[2].transform.position.z - (0.6f * 2f));
 			}else{
 				blocks[2].color = new Color(blocks[2].color.r, blocks[2].color.g, blocks[2].color.b, blocks[2].color.a + (1f / 15f));
 				allow.color = new Color(allow.color.r, allow.color.g, allow.color.b, allow.color.a - (1f / 15f));
 			}
 
 			if(frame == 360){
-				allow.transform.position = new Vector3(allow.transform.position.x, allow.transform.position.y - 1f, allow.transform.position.z);
+				allow.transform.position = new Vector3(allow.transform.position.x, allow.transform.position.y, allow.transform.position.z - 1f);
 				rollButton.enabled = false;
 			}
 		}
@@ -105,9 +105,9 @@ public class Tutorial_Select : MonoBehaviour {
 			blocks[2].color = new Color(blocks[2].color.r, blocks[2].color.g, blocks[2].color.b, 1f);
 
 			allow.transform.position = new Vector3(1.1f, -0.2f, 0f);
-			blocks[0].transform.position = new Vector3(-1f, 0.6f, 0f);
+			blocks[0].transform.position = new Vector3(-1f, 0f, 0.6f);
 			blocks[1].transform.position = new Vector3(-1f, 0f, 0f);
-			blocks[2].transform.position = new Vector3(-1f, -0.6f, 0f);
+			blocks[2].transform.position = new Vector3(-1f, 0f, -0.6f);
 			frame = 0;
 		}
 	}
