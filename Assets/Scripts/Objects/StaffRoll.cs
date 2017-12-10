6ef0e6ac44a8e72fe9f6ab15d39494a02a835117
ukcat5503 <git@furuyama.tech>
2017-12-11 02:00:52 +0900

    スタッフロールをフェードインするように

--- Assets/Scripts/Objects/StaffRoll.cs
+++ Assets/Scripts/Objects/StaffRoll.cs
@@ -9,13 +9,21 @@ public class StaffRoll : MonoBehaviour {
 	GameObject textObj;
 	GameObject canvas;
 
+	Text obj;
+
 
 	// Use this for initialization
 	void Start () {
 		canvas = GameObject.Find("Canvas");
-		var obj = Instantiate(textObj, transform.position, Quaternion.identity).GetComponent<Text>();
+		obj = Instantiate(textObj, transform.position, Quaternion.identity).GetComponent<Text>();
 		obj.transform.SetParent(canvas.transform);
 		obj.transform.localPosition = textObj.transform.position;
-		
+		obj.color = new Color(1,1,1,0);
+	}
+
+	void Update(){
+		if(obj.color.a < 1f){
+			obj.color = new Color(obj.color.r, obj.color.g, obj.color.b, obj.color.a + 0.005f);
+		}
 	}
 }
