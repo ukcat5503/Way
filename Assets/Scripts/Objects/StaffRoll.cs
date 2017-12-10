e5109eae8d3b082d37becd3ec661e64667f2dfa7
ukcat5503 <git@furuyama.tech>
2017-12-11 01:58:17 +0900

    スタッフロールっぽいのを表示するように

---
+++ Assets/Scripts/Objects/StaffRoll.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StaffRoll : MonoBehaviour {
+
+	[SerializeField]
+	GameObject textObj;
+	GameObject canvas;
+
+
+	// Use this for initialization
+	void Start () {
+		canvas = GameObject.Find("Canvas");
+		var obj = Instantiate(textObj, transform.position, Quaternion.identity).GetComponent<Text>();
+		obj.transform.SetParent(canvas.transform);
+		obj.transform.localPosition = textObj.transform.position;
+		
+	}
+}
